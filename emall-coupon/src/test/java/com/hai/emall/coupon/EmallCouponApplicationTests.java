0f148f1c036ac96b3fc0cf7b19aa155c02ec9af8
seay98 <yuhai_2020@hotmail.com>
2023-10-20 16:41:46 +0200

    Built initial construction

---
+++ emall-coupon/src/test/java/com/hai/emall/coupon/EmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hai.emall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ emall-order/src/test/java/com/hai/emall/order/EmallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hai.emall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
